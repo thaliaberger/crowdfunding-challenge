156300e37a1226db56584c991068171bc6fe81db
Thalía <thaliaberger@hotmail.com>
2021-03-17 22:31:02 -0300

    first buttons working

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import logo from "../images/logo-mastercraft.svg";
 import bookmark from "../images/icon-bookmark.svg";
+import close from "../images/icon-close-modal.svg";
 
 function Homepage() {
+  const [totalbacked, setTotalBacked] = useState(89.914);
+  const [totalbackers, setTotalBackers] = useState(5.007);
+  const [bambooStand, setBambooStand] = useState(101);
+  const [blackEditionStand, setBlackEditionStand] = useState(64);
+  const [bookmarkToggle, setBookmarkToggle] = useState("Bookmark");
+  const [modalToggle, setModalToggle] = useState(false);
+
+  function handleBookmarkToggle() {
+    bookmarkToggle === "Bookmark"
+      ? setBookmarkToggle("Bookmarked")
+      : setBookmarkToggle("Bookmark");
+  }
+
+  function handleModal() {
+    setModalToggle(!modalToggle);
+  }
+
   return (
     <main>
       <section>
@@ -13,49 +31,130 @@ function Homepage() {
           A beautiful & handcrafted monitor stand to reduce neck and eye strain
         </p>
         <div className="buttons">
-          <p>Back this project</p>
-          <p>
+          <p onClick={handleModal}>Back this project</p>
+          <p
+            onClick={handleBookmarkToggle}
+            style={{
+              color: `${
+                bookmarkToggle === "Bookmark"
+                  ? "hsl(0, 0%, 48%)"
+                  : "hsl(176, 72%, 28%)"
+              }`,
+            }}
+          >
             <img src={bookmark} />
-            Bookmark
+            {bookmarkToggle}
           </p>
         </div>
       </section>
 
       <section className="numbers-section">
-        <div className="numbers-div">
-          <h4>$89,914</h4> <p>of $100,000 backed</p>
+        <div className="numbers-divs">
+          <div className="numbers-div">
+            <h4>${totalbacked}</h4> <p>of $100,000 backed</p>
+          </div>
+          <div className="numbers-div">
+            <h4>{totalbackers} </h4>
+            <p>total backers</p>
+          </div>
+          <div className="numbers-div">
+            <h4>56</h4> <p>days left</p>
+          </div>
         </div>
-        <div className="numbers-div">
-          <h4>5,007 </h4>
-          <p>total backers</p>
-        </div>
-        <div className="numbers-div">
-          <h4>56</h4> <p>days left</p>
+        <div className="progress-bar-container">
+          <div
+            className="progress-bar-content"
+            style={{ width: `${Math.floor(totalbacked)}%` }}
+          ></div>
         </div>
       </section>
 
-      <section>
+      <section className="options-section">
         <h3>About this project</h3>
         <p>
-          Duis irure deserunt nulla velit pariatur aute voluptate exercitation
-          nulla magna. Quis anim dolore magna laboris amet ut. Velit anim ut
-          duis magna officia Lorem ullamco. Occaecat fugiat dolor aliquip fugiat
-          consequat labore nostrud nulla voluptate ullamco. Id dolore anim magna
-          magna sit veniam. Irure sit magna laboris cillum enim officia pariatur
-          quis. Ad est adipisicing commodo voluptate eiusmod. Eiusmod irure ad
-          incididunt amet labore elit nisi dolore labore amet. Do exercitation
-          in ea ea esse laboris fugiat laboris et labore. Esse cupidatat
-          consectetur duis proident. Quis incididunt duis aliquip ut in
-          excepteur. Veniam do enim amet aute proident. Exercitation cillum enim
-          cupidatat voluptate laborum officia do non. In deserunt sunt nostrud
-          aute cillum. Ad esse laborum dolore nulla consequat velit. Esse amet
-          culpa nostrud sint voluptate cupidatat culpa do irure consequat.
+          The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform
+          that elevates your screen to a more comfortable viewing height.
+          Placing your monitor at eye level has the potential to improve your
+          posture and make you more comfortable while at work, helping you stay
+          focused on the task at hand.
+        </p>
+        <p>
+          Featuring artisan craftsmanship, the simplicity of design creates
+          extra desk space below your computer to allow notepads, pens, and USB
+          sticks to be stored under the stand.
         </p>
 
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <div>
+            <h3>Bamboo Stand </h3> <h5>Pledge $25 or more</h5>
+          </div>
+          <div>
+            You get an ergonomic stand made of natural bamboo. You've helped us
+            launch our promotional campaign, and you’ll be added to a special
+            Backer member list.
+          </div>
+          <div>
+            <p>
+              <strong> {bambooStand}</strong> left
+            </p>{" "}
+            <button>Select Reward</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Black Edition Stand </h3> <h5>Pledge $75 or more </h5>
+          </div>
+          <div>
+            You get a Black Special Edition computer stand and a personal thank
+            you. You’ll be added to our Backer member list. Shipping is
+            included.
+          </div>
+          <div>
+            <p>
+              <strong>{blackEditionStand}</strong> left
+            </p>{" "}
+            <button>Select Reward</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h3>Mahogany Special Edition </h3> <h5>Pledge $200 or more</h5>
+          </div>
+          <div>
+            You get two Special Edition Mahogany stands, a Backer T-Shirt, and a
+            personal thank you. You’ll be added to our Backer member list.
+            Shipping is included.
+          </div>
+          <div>
+            <p>
+              <strong>0</strong> left
+            </p>{" "}
+            <button disabled>Out of Stock</button>
+          </div>
+        </div>
       </section>
+      <div
+        className="overlay"
+        style={{ display: `${modalToggle ? "block" : "none"}` }}
+      >
+        <section className="options-modal-section">
+          <img onClick={handleModal} src={close} />
+          Back this project Want to support us in bringing Mastercraft Bamboo
+          Monitor Riser out in the world? Pledge with no reward Choose to
+          support us without a reward if you simply believe in our project. As a
+          backer, you will be signed up to receive product updates via email.
+          Bamboo Stand Pledge $25 or more You get an ergonomic stand made of
+          natural bamboo. You've helped us launch our promotional campaign, and
+          you’ll be added to a special Backer member list. 101 left Enter your
+          pledge $25 Continue Black Edition Stand Pledge $75 or more You get a
+          Black Special Edition computer stand and a personal thank you. You’ll
+          be added to our Backer member list. Shipping is included. 64 left
+          Enter your pledge $75 Continue Mahogany Special Edition Pledge $200 or
+          more You get two Special Edition Mahogany stands, a Backer T-Shirt,
+          and a personal thank you. You’ll be added to our Backer member list.
+          Shipping is included. 0 left Enter your pledge $200 Continue
+        </section>
+      </div>
     </main>
   );
 }
